47ba4d6f1b0495a94b74144b69c85a515ad685a6
muqtar6954 <muqtarabdiaziz8@gmail.com>
2023-07-07 16:04:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+menu=document.querySelector(".lines");
+menu.onclick=function(){
+    navbar=document.querySelector(".navbar");
+        navbar.classList.toggle("active")
+
+        intro=document.querySelector(".intro");
+            intro.classList.toggle("active2")
+    }
